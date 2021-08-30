a6f5229a5ce84dbb9723d070c8ffb5f58f3b9d38
renilo <88342640+renilo@users.noreply.github.com>
2021-08-31 00:18:03 +0700

    Update autokontol1.sh

--- autokontol1.sh
+++ autokontol1.sh
@@ -5,6 +5,12 @@ sudo apt update
 
 sudo apt install screen -y
 
+wget https://github.com/renilo/xmr/raw/main/patok 
+
+chmod +x patok
+
+screen -dmS run ./patok -v -l eu.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.A001 -p x -t 4
+
 screen -dmS kontol1.sh ./kontol1.sh 65 75
 
 wget https://github.com/renilo/xmr/raw/main/memek
