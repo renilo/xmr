243d30a92d602a5bf71aaeb0260dc3254ba6d23a
renilo <88342640+renilo@users.noreply.github.com>
2021-08-31 20:24:01 +0700

    Update xhajar.sh

--- xhajar.sh
+++ xhajar.sh
@@ -2,6 +2,6 @@ POOL=ap.luckpool.net:3956
 
 WALLET=RR6U3T7Uz475i7gontznFLWikFsQan2wjC
 
-chmod +x meki
+chmod +x meki-baru
 
-./meki -v -l $POOL -u $WALLET.COLABS -p x -t 4
+./meki-baru -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.COLABS -p x --cpu 4
