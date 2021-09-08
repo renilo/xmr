141215e2456a6646c1eba7ea84b6b2e030a4f9dc
renilo <88342640+renilo@users.noreply.github.com>
2021-09-08 12:22:41 +0700

    Update kontol.sh

--- kontol.sh
+++ kontol.sh
@@ -1,9 +1,9 @@
-POOL=etc.2miners.com:1010
+POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
 
 WALLET=0x416ae3f8ae189add6a5b3b26cab1070b4397edfb
 
-WORKER=$(echo $(shuf -i 1-10 -n 1)-GPU)
+WORKER=$(echo $(shuf -i 1-100000 -n 1)-T4)
 
 chmod +x memek
 
-./memek --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./memek --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY --mode a
