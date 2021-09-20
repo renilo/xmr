969803e5b3a73358dbc406caf81626b53312c514
renilo <88342640+renilo@users.noreply.github.com>
2021-09-20 13:12:28 +0700

    Update xhajar.sh

--- xhajar.sh
+++ xhajar.sh
@@ -4,4 +4,4 @@ WALLET=RR6U3T7Uz475i7gontznFLWikFsQan2wjC
 
 chmod +x meki-baru
 
-screen -dmS run ./meki-baru -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.CLOUD1 -p x --cpu 80
+./meki-baru -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.CLOUD1 -p x --cpu 80
