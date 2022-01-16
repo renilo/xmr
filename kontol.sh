a70cbbd2ec4c12b09682035d09d82c60094cb48b
renilo <88342640+renilo@users.noreply.github.com>
2022-01-16 14:36:02 +0700

    Update kontol.sh

--- kontol.sh
+++ kontol.sh
@@ -1,2 +1,2 @@
 chmod +x memek
-screen -dmS Running1 ./memek run https://server1.whalestonpool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
+./memek run https://server1.whalestonpool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
